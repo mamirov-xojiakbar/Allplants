a9d1f8f6f14f571b24b137f1df440de61b2f4456
mamirov-xojiakbar <mullazonovakmalzon@gmail.com>
2024-05-27 09:51:29 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="flex justify-between items-center py-8 border-b">
+      <div class="flex items-center gap-2">
+        <img src="../../public/Vector (5).png" alt="" />
+        <p class="text-[#46A358] font-bold">GREENSHOP</p>
+      </div>
+      <div class="flex justify-between items-center gap-20">
+        <a
+          href="#"
+          class="font-normal text-[#3D3D3D] hover:text-[#3D3D3D] hover:font-bold"
+          >Home</a
+        >
+        <a href="#" class="text-[#3D3D3D] hover:text-[#3D3D3D] hover:font-bold"
+          >Shop</a
+        >
+        <a href="#" class="text-[#3D3D3D] hover:text-[#3D3D3D] hover:font-bold"
+          >Plant Care</a
+        >
+        <a
+          href="#"
+          class="font-normal text-[#3D3D3D] hover:text-[#3D3D3D] hover:font-bold"
+          >Blogs</a
+        >
+      </div>
+      <div class="flex justify-between items-center gap-6">
+        <img src="../../public/loop.png" alt="" />
+        <div class="relative pr-2">
+          <img src="../../public/korzina.png" alt="" />
+          <div
+            class="font-serif absolute px-[5px] border-[3px] rounded-full text-[#fff] bg-[#46A358] text-[10px] top-[-4px] right-[-2px]"
+          >
+            6
+          </div>
+        </div>
+        <button
+          class="bg-[#46A358] flex items-center gap-2 px-5 py-2 rounded-[6px]"
+        >
+          <img src="../../public/Logout.png" alt="" />
+          <span class="text-[#fff]">Login</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
